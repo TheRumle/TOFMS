bc7e8b9e385a68864cd7ef1f3ce497fb910fe23e
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2023-11-16 14:01:18 +0100

    Moved types into own files

---
+++ TACPN/CapacityPlaceCreator.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using TACPN.Net;
+
+namespace TACPN;
+
+public static class CapacityPlaceCreator
+{
+
+    public static readonly string DefaultColorName = "dot";
+    public static string CapacityNameFor(Place place)
+    {
+        return place.Name + "_buffer";
+    }
+    
+    public static string CapacityNameFor(string name)
+    {
+        return name + "_buffer";
+    }
+    
+    public static Place CapacityPlaceFor(Place place)
+    {
+        var infinityPlaces = place.ColorInvariants.Select(kvp => 
+            new KeyValuePair<string, int>(DefaultColorName, InfinityInteger.Positive));
+        return new Place(CapacityNameFor(place), infinityPlaces);
+    }
+}
--- TACPN/TofmToTacpnTranslater.cs
+++ TACPN/TofmToTacpnTranslater.cs
@@ -1,32 +1,9 @@
-﻿using Common;
-using Common.Move;
+﻿using Common.Move;
 using Common.Translate;
 using TACPN.Net;
 using TACPN.Net.Transitions;
 
 namespace TACPN;
-
-public static class CapacityPlaceCreator
-{
-
-    public static readonly string DefaultColorName = "dot";
-    public static string CapacityNameFor(Place place)
-    {
-        return place.Name + "_buffer";
-    }
-    
-    public static string CapacityNameFor(string name)
-    {
-        return name + "_buffer";
-    }
-    
-    public static Place CapacityPlaceFor(Place place)
-    {
-        var infinityPlaces = place.ColorInvariants.Select(kvp => 
-            new KeyValuePair<string, int>(DefaultColorName, InfinityInteger.Positive));
-        return new Place(CapacityNameFor(place), infinityPlaces);
-    }
-}
 
 public class TofmToTacpnTranslater : IMoveActionTranslation<PetriNetComponent>
 {
