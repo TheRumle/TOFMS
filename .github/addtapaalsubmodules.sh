d419b423f2a9e4f99affa3b96a6d2a9d56273ae7
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2023-11-19 12:35:36 +0100

    Submodules

---
+++ .github/addtapaalsubmodules.sh
@@ -0,0 +1,3 @@
+﻿git submodule add https://github.com/tapaal/verifydtapn ./tapaal/verifydtapn
+git submodule add https://github.com/TAPAAL/unfoldTACPN ./tapaal/unfoldTACPN
+git submodule add https://github.com/tapaal/tapaal-gui  ./tapaal/gui
