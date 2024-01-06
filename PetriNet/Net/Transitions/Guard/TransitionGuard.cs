7f99d07c63427f87515e153a5cc43f8cbd3773ed
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2024-01-06 19:10:18 +0100

    Deleted unused code

--- PetriNet/Net/Transitions/Guard/TransitionGuard.cs
+++ PetriNet/Net/Transitions/Guard/TransitionGuard.cs
@@ -7,7 +7,7 @@ namespace TACPN.Net.Transitions.Guard;
 
 public class TransitionGuard//TODO this needs to be conjunction or disjunction
 {
-    public TransitionGuard(ColourType colourType)
+    private TransitionGuard(ColourType colourType)
     {
         ColourType = colourType;
     }
@@ -15,20 +15,7 @@ public class TransitionGuard//TODO this needs to be conjunction or disjunction
     public ColourType ColourType { get; }
     private List<ColourComparison> _colourComparisons = new();
     public IReadOnlyCollection<ColourComparison> Predicates => _colourComparisons.AsReadOnly();
-
-    public void AddComparison(Colour lhs, ColourComparison comparator, Colour rhs)
-    {
-        ColourType.MustContain(lhs);
-        ColourType.MustContain(rhs);
-        _colourComparisons.Add(comparator);
-    }
     
-    public void AddComparison(ColourVariable lhs, ColourComparison comparator, int rhs)
-    {
-        ColourType.MustBeCompatibleWith(lhs);
-        lhs.MustBeAssignableTo(rhs);
-        _colourComparisons.Add(comparator);
-    }
     
     public void AddComparison<T>(ColourComparison<T> comparator) where T : Colour
     {
@@ -38,10 +25,7 @@ public class TransitionGuard//TODO this needs to be conjunction or disjunction
         ColourType.MustContain(rhs);
         _colourComparisons.Add(comparator);
     }
-    public void AddComparison<T>(IEnumerable<ColourComparison<T>> comparisons)  where T : Colour
-    {
-        foreach (var comparison in comparisons) AddComparison(comparison);
-    }
+
 
     public static TransitionGuard Empty(ColourType colourType) => new(colourType);
 
