13a308ca5de576f3e7cfc711138a5b445d65b2b9
Rasmus Hoeyer Hansen <rasmus.hoeyer.hansen@gmail.com>
2024-02-20 12:40:08 +0100

    Test delay making

--- TmpmsScheduler.UnitTest/Algorithm/ConfigurationGeneration/ConfigurationGeneratorTest.cs
+++ TmpmsScheduler.UnitTest/Algorithm/ConfigurationGeneration/ConfigurationGeneratorTest.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
 using FluentAssertions;
 using JetBrains.Annotations;
@@ -25,31 +24,99 @@ public class ConfigurationGeneratorTest
         return ConfigurationGenerator.WithDefaultImplementations(actions);
     }
 
-    
-    [Theory]
-    [InlineData(1,1)]
-    [InlineData(0,1)]
-    [InlineData(0,2)]
-    public void WhenOnlyDelayCanBeMade_ShouldReturnOneConfiguration_ReachableByDelay(int min, int max)
+    [Fact]
+    public void WhenNoDelaysCanBeMade_ShouldReturnEmpty()
     {
-        var location = _locationGenerator.GetRegular() 
-            with
-            {
-                Invariants = new HashSet<Invariant>([new Invariant(PARTTYPE, min, max)]),
-                Configuration = _configurationFactory.SingletonOfAge(PARTTYPE, [], max-1)
-            };
-        
+        int min = 1; int max = 1;
+        var location = CreateSingletonConfigWithAge(min, max, max);
         
         Configuration configurations = CreateConfiguration([location]);
 
         ReachableConfig[] reachableStates = GetGenerator([]).GenerateConfigurations(configurations);
-        reachableStates.Should().HaveCount(1);
-        reachableStates.First().TimeCost().Should().Be(1);
+        reachableStates.Should().BeEmpty();
     }
+
+
+
+    [Theory]
+    [InlineData(1,1)]
+    [InlineData(0,1)]
+    [InlineData(0,2)]
+    [InlineData(3,4)]
+    public void WhenOnlyOneDelayCanBeMade_ShouldReturnOneConfiguration_ReachableByDelay1(int min, int max)
+    {
+        var location = CreateSingletonConfigWithAge(min,max , max - 1);
+        Configuration configurations = CreateConfiguration([location]);
+
+        ReachableConfig[] reachableStates = GetGenerator([]).GenerateConfigurations(configurations);
+        reachableStates.Should().HaveCount(1);
+        reachableStates.First().TimeCost.Should().Be(1);
+    }
+    
+    [Theory]
+    [InlineData(1,3, 1)]
+    [InlineData(0,3, 1)]
+    [InlineData(0,2, 0)]
+    public void WhenTwoDelaysCanBeMade_ShouldReturnTwoConfigurations(int min, int max, int age)
+    {
+        var location = CreateSingletonConfigWithAge(min, max, age);
+        
+        Configuration configurations = CreateConfiguration([location]);
+        
+        ReachableConfig[] reachableStates = GetGenerator([]).GenerateConfigurations(configurations);
+        reachableStates.Should().HaveCount(2);
+    }
+    
+    [Theory]
+    [InlineData(0,3, 1, 2)] //Delay to reach 2,3
+    [InlineData(1,3, 1, 2)] //Delay to reach age 2,3
+    [InlineData(1,10, 1, 9)] //Delay to reach age 2,3,..,10
+    [InlineData(1,10, 5, 5)] //Delay to reach age 6,7,8,9,10
+    [InlineData(1,10, 10, 0)] //No delay possible
+    public void WhenAgeBetweenInvariant_ShouldGiveExpectedNumberOfPossibleDelays(int min, int max, int age, int expected)
+    {
+        var location = CreateSingletonConfigWithAge(min, max, age);
+        
+        Configuration configurations = CreateConfiguration([location]);
+        
+        ReachableConfig[] reachableStates = GetGenerator([]).GenerateConfigurations(configurations);
+        reachableStates.Should().HaveCount(expected);
+    }
+    
+    [Theory]
+    [InlineData(5,10, 0, 6)] //Delay to reach 5,6,7,8,9,10
+    [InlineData(5,10, 1, 6)] //Delay to reach 5,6,7,8,9,10
+    [InlineData(5,10, 2, 6)] //Delay to reach 5,6,7,8,9,10
+    [InlineData(5,10, 4, 6)] //Delay to reach 5,6,7,8,9,10
+    [InlineData(5,10, 5, 5)] //Delay to reach 6,7,8,9,10
+    [InlineData(10,10, 0, 1)] //Delay to reach 10
+    public void WhenAgeUnderInvariant_ShouldGiveExpectedNumberOfPossibleDelays(int min, int max, int age, int expected)
+    {
+        var location = CreateSingletonConfigWithAge(min, max, age);
+        
+        Configuration configurations = CreateConfiguration([location]);
+        
+        ReachableConfig[] reachableStates = GetGenerator([]).GenerateConfigurations(configurations);
+        reachableStates.Should().HaveCount(expected);
+    }
+    
+    
+    
 
     private Configuration CreateConfiguration(IEnumerable<Location> locations)
     {
         ConfigurationFactory factory = new ConfigurationFactory();
         return factory.From(locations.Select(e=>(e,e.Configuration)));
     }
+    
+    private Location CreateSingletonConfigWithAge(int min, int max, int age)
+    {
+        return _locationGenerator.GetRegular() 
+            with
+            {
+                Invariants = new HashSet<Invariant>([new Invariant(PARTTYPE, min, max)]),
+                Configuration = _configurationFactory.SingletonOfAge(PARTTYPE, [], age)
+            };
+    }
+
 }
--- TmpmsScheduler/Algorithm/ConfigurationGeneration/ConfigurationGenerator.cs
+++ TmpmsScheduler/Algorithm/ConfigurationGeneration/ConfigurationGenerator.cs
@@ -47,28 +47,48 @@ internal class ConfigurationGenerator : IConfigurationGenerator
     private IEnumerable<ReachableConfig> ComputePossibleDelays(Configuration configuration)
     {
         return PossibleDelaySpan(configuration) switch
-        {
-            (_, <= 0) => [],
-            var (minimumTime, maxPossibleDelay) =>
-                Enumerable
-                    .Range(minimumTime, minimumTime - maxPossibleDelay)
-                    .Select(delay => new ReachableConfig(delay, _actionExecutor.Delay(delay, configuration)))
+        {   
+            (<0,_) => throw new ArgumentException($"Min delay can never be smaller than 0."),
+            (_,<0) => throw new ArgumentException($"Max delay can never be smaller than 0."),
+            (_, 0 ) => [],
+            (_, 1) => [CreateSingle(1, configuration)],
+            (0, var maxDelay ) => CreateRange(1, maxDelay, configuration),
+            (var minDelay, var maxDelay) => CreateRange(minDelay, maxDelay, configuration),
         };
     }
 
+    private IEnumerable<ReachableConfig> CreateRange(int minDelay, int maxDelay, Configuration configuration)
+    {
+        if (minDelay == maxDelay) return 
+            [new ReachableConfig(maxDelay, _actionExecutor.Delay(maxDelay, configuration))];
+        
+        return Enumerable
+            .Range(minDelay, (maxDelay-minDelay)+1)
+            .Select(i=> new ReachableConfig(i, _actionExecutor.Delay(i, configuration)));
+    }
+
+    private ReachableConfig CreateSingle(int i, Configuration configuration)
+    {
+        return new ReachableConfig(i, _actionExecutor.Delay(i, configuration));
+    }
+
+
     private (int TimeUntilMinReached, int MaxDelay) PossibleDelaySpan(Configuration configuration)
     {
-        var maxDelay = 0;
-        var timeUntilMinReached = 0;
+        var maxDelay = Int32.MaxValue;
+        var timeUntilNextMinInvariantReached = maxDelay;
+        
         foreach (var (location, locationConfig) in configuration.LocationConfigurations)
             foreach (var (partType, invariant) in location.InvariantsByType)
                 foreach (var part in locationConfig.PartsByType[partType])
                 {
                     //If any part is at the max we cannot perform any delay
                     if (invariant.Max == part.Age) return (0, 0);
+                    
                     maxDelay = Math.Min(maxDelay, invariant.Max - part.Age);
-                    timeUntilMinReached = Math.Min(timeUntilMinReached, invariant.Min - part.Age);
+                    timeUntilNextMinInvariantReached = Math.Min(timeUntilNextMinInvariantReached, Math.Max(invariant.Min - part.Age, 0));
                 }
-        return (timeUntilMinReached, maxDelay);
+        
+        return (timeUntilNextMinInvariantReached, maxDelay);
     }
 }
